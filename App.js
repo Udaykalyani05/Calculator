7f46123f4dc64fd24298a5f85a5e8cc6a34c18db
KANISETTI UDAY KALYANI <130745849+Udaykalyani05@users.noreply.github.com>
2023-06-24 23:45:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import UserCardGrid from './components/UserCardGrid';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <UserCardGrid />
+    </div>
+  );
+};
+
+export default App;
